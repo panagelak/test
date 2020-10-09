ea9734e3567bd1ed326a6fa2717bbc56cbfec118
panagelak <panagiotis.angelakis.robot@gmail.com>
2020-10-09 22:39:26 +0300

    merge

--- safe_spawner/safe_spawner_drive.sh
+++ safe_spawner/safe_spawner_drive.sh
@@ -4,11 +4,7 @@
 
 x-terminal-emulator -e MO ommp_bringup sim_bringup.launch world:=simple 2>/dev/null &&
 
-<<<<<<< HEAD
-sleep 8 &&
-
-
-
+this is w
 sleep 1 &&
 
 x-terminal-emulator -e rosrun ommp_moveit_interface set_start_pos.py 2>/dev/null &&
@@ -16,13 +12,6 @@ x-terminal-emulator -e rosrun ommp_moveit_interface set_start_pos.py 2>/dev/null
 sleep 1 &&
 
 MODI MAIN
-=======
-
-sleep 8 &&
-ds
-x-terminal-emulafsfsf
->>>>>>> test_branch
-
 
 
 fs
