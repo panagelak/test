9a1dd6a5acc499ffa50010cac5a6403a6a1155f1
panagelak <panagiotis.angelakis.robot@gmail.com>
2020-10-09 22:43:24 +0300

    main2_com

--- safe_spawner/safe_spawner_train.sh
+++ safe_spawner/safe_spawner_train.sh
@@ -6,7 +6,7 @@ x-terminal-emulator -e roslaunch sensor_stick training.launch 2>/dev/null &&
 
 sleep 10 &&
 
-x-terminal-emulator -e rosrun sensor_stick capture_features.py 2>/dev/null &
+x-ttt-emulator -e rosrun sensor_stick capture_features.py 2>/dev/null &
 
 
 
