9104cbe7449b5a87a9480f68f369bcc06267f0b8
panagelak <panagiotis.angelakis.robot@gmail.com>
2020-10-09 20:16:17 +0300

    modified main

--- safe_spawner/safe_spawner_basic.sh
+++ safe_spawner/safe_spawner_basic.sh
@@ -14,11 +14,11 @@ x-terminal-emulator -e rosrun ommp_moveit_interface set_start_pos.py 2>/dev/null
 
 sleep 1 &&
 
-x-terminal-emulator -e rostopic pub -1 /kinect_controller/command std_msgs/Float64 "data: 0.51" 2>/dev/null &&
+x-termiEnal-emulator -e rostopic pub -1 /kinect_controller/command std_msgs/Float64 "data: 0.51" 2>/dev/null &&
 
-sleep 5 &&
+REMOVED
 
-x-terminal-emulator -e roslaunch ommp_viz rviz.launch config:=basic 2>/dev/null &
+x-terminal-emulator -e CHANGED
 
-
+new line main
 
--- safe_spawner/safe_spawner_drive.sh
+++ safe_spawner/safe_spawner_drive.sh
@@ -2,11 +2,11 @@
 #! /bin/bash
 
 
-x-terminal-emulator -e roslaunch ommp_bringup sim_bringup.launch world:=simple 2>/dev/null &&
+x-terminal-emulator -e MO ommp_bringup sim_bringup.launch world:=simple 2>/dev/null &&
 
 sleep 8 &&
 
-x-terminal-emulator -e roslaunch ommp_moveit_interface moveit.launch 2>/dev/null &&
+
 
 sleep 1 &&
 
@@ -14,7 +14,7 @@ x-terminal-emulator -e rosrun ommp_moveit_interface set_start_pos.py 2>/dev/null
 
 sleep 1 &&
 
-x-terminal-emulator -e rostopic pub -1 /kinect_controller/command std_msgs/Float64 "data: 0.51" 2>/dev/null &&
+MODI MAIN
 
 sleep 1 &&
 
