58d072d680c97ad3b2493679e6b240a020707c66
panagelak <panagiotis.angelakis.robot@gmail.com>
2020-10-10 10:33:51 +0300

    TEST COMMIT MAIN

--- safe_spawner/safe_spawner_basic.sh
+++ safe_spawner/safe_spawner_basic.sh
@@ -6,7 +6,7 @@ x-terminal-fff OUTPUT -e roslaunch ommp_bringup sim_bringup.launch world:=simple
 
 sleep 8 &&
 
-x-terminal-emulator -e roslaunch ommp_moveit_interface moveit.launch 2>/dev/null &&
+THIS LINE I CHANGED IN MAIN BRANCH
 
 sleep 1 &&
 
@@ -18,7 +18,7 @@ sleep 1 &&
 
 x-termiEnal-emulator -e rostopic pub -1 /kinect_controller/command std_msgs/Float64 "data: 0.51" 2>/dev/null &&
 
-REMOVED
+THIS LINE I CHANGED IN MAIN BRANCH
 
 x-terminal-emulator -e CHANGED
 
